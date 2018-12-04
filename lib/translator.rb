2302017c7e3878753bf4bd2f3e1d5bd037c55b07
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-12-04 04:57:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  x = YAML.load_file(path)
+  x
+  binding.pry
 end
 
 def get_japanese_emoticon
